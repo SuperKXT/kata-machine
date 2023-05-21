55496f9b2b58f46a23a53d840c744978d10b80ef
SuperKXT <superkxt@outlook.com>
2023-05-21 17:12:51 +0500

    Add solution for `TwoCrystalBalls`

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,13 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
-
+    let low = 0;
+    while (low <= breaks.length) {
+        const jump = Math.floor(low + Math.sqrt(breaks.length - low));
+        if (breaks[jump]) {
+            for (let i = low; i <= jump; i++) {
+                if (breaks[i]) return i;
+            }
+        }
+        low = jump + 1;
+    }
+    return -1;
 }
