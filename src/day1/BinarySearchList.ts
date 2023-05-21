5f08c6accfc7d2f8d407041d525771a8c15a270f
SuperKXT <superkxt@outlook.com>
2023-05-21 16:47:38 +0500

    Add solution for `BinarySearchList`

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,11 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-
+    let start = 0;
+    let end = haystack.length;
+    while (start !== end) {
+        const middle = Math.floor(start + (end - start) / 2);
+        if (haystack[middle] === needle) return true;
+        if (haystack[middle] > needle) end = middle;
+        else start = middle + 1;
+    }
+    return false;
 }
