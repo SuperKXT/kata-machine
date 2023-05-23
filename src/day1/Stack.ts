f76c077eeac8eb34b1752879463b1c6438dd1ed9
SuperKXT <superkxt@outlook.com>
2023-05-23 12:24:27 +0500

    Implement `Stack`

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,31 @@
+type Node<T> = {
+    item: T;
+    next?: Node<T>;
+};
 export default class Stack<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
 
     constructor() {
+        this.length = 0;
+        this.head = undefined;
     }
 
     push(item: T): void {
+        const node = { item, next: this.head };
+        this.head = node;
+        this.length++;
+    }
 
-}
     pop(): T | undefined {
+        if (!this.head) return undefined;
+        const head = this.head;
+        this.head = head.next;
+        this.length--;
+        return head?.item;
+    }
 
-}
     peek(): T | undefined {
-
-}
+        return this.head?.item;
+    }
 }
