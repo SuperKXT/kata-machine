a5217dd654924eaeac79405be0199e9a7afb0b39
SuperKXT <superkxt@outlook.com>
2023-05-23 11:19:53 +0500

    Update `BubbleSort`

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,13 +1,9 @@
-export default function bubble_sort(
-	arr: number[]
-): void {
-	let end = arr.length;
-	while(end > 1) {
-		for (let i = 0; i < end; i++) {
-			if (arr[i] > arr[i + 1]) {
-				[arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
-			}
-		}
-		end--;
-	}
+export default function bubble_sort(arr: number[]): void {
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = 0; j < arr.length - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+            }
+        }
+    }
 }
