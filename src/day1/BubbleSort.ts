daa047f2652d87ede90aee03b24d50c0e053cb9c
SuperKXT <superkxt@outlook.com>
2023-05-23 11:14:11 +0500

    Solve `BubbleSort`

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,13 @@
-export default function bubble_sort(arr: number[]): void {
-
+export default function bubble_sort(
+	arr: number[]
+): void {
+	let end = arr.length;
+	while(end > 1) {
+		for (let i = 0; i < end; i++) {
+			if (arr[i] > arr[i + 1]) {
+				[arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+			}
+		}
+		end--;
+	}
 }
