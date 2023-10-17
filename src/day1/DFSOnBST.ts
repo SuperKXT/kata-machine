beaf3da147c7acd6497efd28e6407c7d0c0175d4
SuperKXT <superkxt@outlook.com>
2023-10-17 14:09:24 +0500

    Implement `DFSOnBST`

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,8 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
-
+export default function dfs(
+    head: BinaryNode<number> | null,
+    needle: number,
+): boolean {
+    if (!head) return false;
+    if (head.value === needle) return true;
+    return dfs(needle > head.value ? head.right : head.left, needle);
 }
